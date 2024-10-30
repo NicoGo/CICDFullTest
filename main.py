4425f327fbe3e925a551ef1bd824b130a628bcde
Nicolas Brandstett <nicolasbrandstett@MacBook-Pro-de-Nicolas.local>
2024-10-30 22:18:12 +0800

    adding intensive cpu task

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ app = FastAPI()
 @app.get("/hello")
 async def hello():
     return {"message": "Hello, w orld now two with some new return !"}
+
+@app.get("/heavy")
+async def hello():
+    import time
+
+    start_time = time.time()
+    while time.time() - start_time < 31:
+        # Perform a heavy computation (e.g., calculate Fibonacci numbers)
+        a, b = 0, 1
+        for _ in range(1000000):  # Adjust this number for more or less CPU load
+            a, b = b, a + b
+
+    return {"Done"}
